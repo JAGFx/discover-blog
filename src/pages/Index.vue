024795ea468efe307c309bd59cbb77a439c2ad52
JAGFx <contact@jagfx.fr>
2021-09-05 16:12:09 +0200

    :tada: initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,78 @@
+<template>
+  <Layout>
+    <header class="header">
+      <img id="logo" src="../../static/Logo-Jagfx-2018BG.png" width="100" height="100" alt="JAGFx">
+      <h1 v-html="$page.metadata.siteName" />
+      <h2 v-html="$page.metadata.siteDescription" />
+    </header>
+    <section class="posts">
+      <PostList
+        v-for="edge in $page.allPost.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+      />
+    </section>
+  </Layout>
+</template>
+
+<script>
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList,
+  },
+  metaInfo: {
+    title: "A simple blog",
+  },
+};
+</script>
+
+<page-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+  allPost {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        timeToRead
+        description
+        date (format: "D MMMM YYYY")
+        path
+      }
+    }
+
+  }
+}
+</page-query>
+
+<style>
+.header {
+  text-align: center;
+  padding: 0.7em;
+}
+
+.header h1  {
+  font-family: "mathilde";
+  text-transform: lowercase;
+  font-size: 7rem;
+  line-height: 0.7em;
+  margin: 1rem auto;
+  font-weight: 500;
+}
+
+.header h2 {
+  font-weight: 200;
+  font-size: 1rem;
+  line-height: 1rem;
+}
+
+.header #logo{
+  border-radius: 5rem;
+  overflow: hidden;
+}
+</style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,54 @@
+<template>
+  <Layout>
+    <br />
+    <g-link to="/" class="link"> &larr; Go Back</g-link>
+    <div class="post-title">
+      <h1>{{ $page.post.title }}</h1>
+      <p class="post-date">
+        {{ $page.post.date }} | {{ $page.post.timeToRead }} min read
+      </p>
+    </div>
+    <hr>
+    <div class="post-content">
+      <p v-html="$page.post.content" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+   post: post (path: $path) {
+    id
+    title
+    content
+    date (format: "D MMMM YYYY")
+    timeToRead
+  }
+}
+</page-query>
+
+<style>
+.post-title {
+  text-align: center;
+}
+
+.post-title h1 {
+  font-family: "mathilde";
+  text-transform: lowercase;
+  font-size: 7rem;
+  line-height: 0.7em;
+  margin: 4rem auto;
+  font-weight: 500;
+}
+
+.post-date {
+  font-weight: 200;
+  font-size: 1rem;
+  line-height: 1rem;
+  margin: 3rem auto;
+}
+
+.post-content {
+  font-size: 20px;
+}
+</style>
